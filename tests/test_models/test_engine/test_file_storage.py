6332c39d4506ff4f28b40add363eccfe52c4ce37
El Madani <123333191+Madani00@users.noreply.github.com>
2024-03-12 14:30:00 +0000

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -38,11 +38,14 @@ class TestFileStorage(unittest.TestCase):
         self.storage.save()
         self.assertTrue(os.path.exists(self.file_path))
 
-    def test_reload(self):
-        self.storage._FileStorage__objects = self.objects
-        self.storage.save()
-        self.storage.reload()
-        self.assertIn("BaseModel.{}".format(self.base_model.id), self.objects)
+    def test_method_reload(self):
+        file_storage = FileStorage()
+        base_model = BaseModel()
+        file_storage.save()
+        models.storage.reload()
+        objects = file_storage._FileStorage__objects
+
+        self.assertIn("BaseModel.{}".format(base_model.id), objects)
 
 if __name__ == "__main__":
     unittest.main()
